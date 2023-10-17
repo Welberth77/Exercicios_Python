ff941a688ce7dfc5bf96b0ad2369e39bd84979c0
Welberth <welbertholiveira250@gmail.com>
2023-10-17 19:52:46 -0300

    Create ex_02.py

---
+++ ex_02.py
@@ -0,0 +1,9 @@
+# Ordena 5 valores dados pelo usuário
+lista = []
+
+for i in range(0, 5):
+    n = int(input(f'Digite o {i+1}° número: '))
+    lista.append(n)
+
+lista.sort()
+print(f'Os valores digitados em ordem foram {lista}')
